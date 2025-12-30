41a1d4042afe1b34ac95c42f5e920e0ef766253f
NoahB6JS <noah.js.b6@gmail.com>
2025-12-30 12:25:37 +0000

    dock at bottom opens apps

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-let zIndex = 20;
-
-function openWindow(id) {
+// Claoing window function
+function closeWindow(id) {
   const win = document.getElementById(id);
-  win.style.display = "block";
-  zIndex++;
-  win.style.zIndex = zIndex;
+
+  win.classList.remove("active");
+  win.style.visibility = "hidden";
 }
 
-function closeWindow(id) {
-  document.getElementById(id).style.display = "none";
+//Opening window function
+function openWindow(id, btn) {
+  const win = document.getElementById(id);
+
+  
+  if (btn) {
+  
+    if (win.classList.contains("active")) {
+      closeWindow(id);
+      return;
+    }
+
+    win.style.visibility = "hidden";
+    win.classList.add("active");
+
+    const winRect = win.getBoundingClientRect();
+    const btnRect = btn.getBoundingClientRect();
+
+
+    win.style.left =
+      btnRect.left + btnRect.width / 2 - winRect.width / 2 + "px";
+
+
+    win.style.top =
+      btnRect.top - winRect.height - 12 + "px";
+
+    win.style.visibility = "visible";
+    return;
+  }
+
+
+  win.style.display = "block";
 }
